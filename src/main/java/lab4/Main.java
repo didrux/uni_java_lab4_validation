3f882824b3292b576903e7eddb8480b99c3bbb06
Michael Ovdiienko <mishaov@gmail.com>
2022-12-15 12:25:51 +0200

    chore: refactor

--- src/main/java/lab4/Main.java
+++ src/main/java/lab4/Main.java
@@ -2,28 +2,5 @@ package lab4;
 
 public class Main {
     public static void main(String[] args) {
-        Employee.EmployeeBuilder employeeBuilder = new Employee.EmployeeBuilder("gsakgddsg Smithers")
-                .setSalary(100.0)
-                .setAddress("")
-                .setPhoneNumber("3809500000009999999")
-                .setIsInsuranced(true);
-
-        Employee employee1 = employeeBuilder.build();
-        System.out.println(employee1);
-
-        Producer.ProducerBuilder producerBuilder = new Producer.ProducerBuilder("fdbeang")
-                .setAddress("Universitetska,20")
-                .setContactPerson(employee1);
-
-        Producer producer1 = producerBuilder.build();
-        System.out.println(producer1);
-
-        Product.ProductBuilder productBuilder = new Product.ProductBuilder(10, "j")
-                .setCategory("gsdfg")
-                .setQuantity(12410)
-                .setPrice(10);
-
-        Product p = productBuilder.build();
-
     }
 }
